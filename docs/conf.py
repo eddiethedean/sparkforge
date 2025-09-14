dee44bfff05032d4b6a8dc29c584d98a010641d8
Odo Matthews <odosmatthews@gmail.com>
2025-09-14 12:56:39 -0400

    Fix Read the Docs build issues
    - Simplify .readthedocs.yml to use automatic build process
    - Remove problematic extensions from requirements.txt
    - Use sphinx-rtd-theme for better Read the Docs compatibility
    - Remove custom build commands that were causing output path issues
    - Let Read the Docs handle the build process automatically

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ release = '0.3.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
@@ -51,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -145,13 +144,9 @@ html_short_title = "SparkForge"
 
 # Theme options
 html_theme_options = {
-    'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': '#2980B9',
-    # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
