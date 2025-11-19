56f65d8ef1e91ffd0258f1ba6401a39026fb49be
Odo Matthews <odosmatthews@gmail.com>
2025-11-19 10:19:40 -0500

    add general interfaces

---
+++ interfaces/builder.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from typing import Protocol
+
+from runner import Runner
+from rules import Rules
+from transformer import Transformer
+
+
+class Builder(Protocol):
+
+    def to_pipeline(self) -> Runner:
+        ...
+
+    def with_bronze_rules(
+        self,
+        name: str,
+        rules: Rules
+    ) -> Builder:
+        ...
+
+    def add_silver_transform(
+        self,
+        name: str,
+        transform: Transformer,
+        rules: Rules
+    ) -> Builder:
+        ...
+    
+    def add_gold_transform(
+        self,
+        name: str,
+        transform: Transformer,
+        rules: Rules
+    ) -> Builder:
+        ...
---
+++ interfaces/runner.py
@@ -0,0 +1,19 @@
+from typing import Protocol
+
+from report import Report
+from sources import Sources
+
+
+class Runner(Protocol):
+    
+    def run_initial_load(
+        self,
+        bronze_sources: Sources
+    ) -> Report:
+        ...
+
+    def run_incremental(
+        self,
+        bronze_sources: Sources
+    ) -> Report:
+        ...
